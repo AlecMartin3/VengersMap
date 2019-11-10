6989544d99685764f5841734891edefb7b63475e
Jeavin Shoker <jshokerwow@gmail.com>
2019-11-10 14:34:44 -0800

    Map changes based on selected park in spinner. Started adding artifact locations

--- app/src/main/java/com/example/vengersmap/AddArtifact.java
+++ app/src/main/java/com/example/vengersmap/AddArtifact.java
@@ -7,26 +7,35 @@ import android.widget.EditText;
 
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+
+import java.util.ArrayList;
 
 public class AddArtifact extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mMap;
     private int position;
     private EditText etArtName;
-    private Artifact a;
     private String name = " ";
+    private String park;
+    private ArrayList<locations> artLocations;
+    public SupportMapFragment mapFragment;
 
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artifact);
+
         position = getIntent().getExtras().getInt("position");
-//        a = (Artifact)getIntent().getSerializableExtra("artifact");
-//        artName = findViewById(R.id.etArtifactName);
-//        name = artName.getText().toString();
+        park = getIntent().getExtras().getString("park");
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -34,9 +43,48 @@ public class AddArtifact extends FragmentActivity implements OnMapReadyCallback,
 
     }
 
+    public void createLocations(){
+        if(park.equals("Strathchona Park")){
+            locations strath1 = new locations();
+            strath1.setName("Point 1");
+            strath1.setX(49.274795);
+            strath1.setY(-123.086702);
+
+            locations strath2 = new locations();
+            strath2.setName("Point 2");
+            strath2.setX(49.276052);
+            strath2.setY(-123.087088);
+            artLocations.add(strath1);
+        }
+        if(park.equals("Jericho Beach Park")){
+
+        }
+        if(park.equals("Musqueam Park")){
+
+        }
+
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if(park.equals("Strathcona Park")){
+            System.out.println(park);
+            LatLng parkLoc = new LatLng(49.274959, -123.085946);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(parkLoc, 16));
+        }
+
+        if(park.equals("Jericho Beach Park")){
+            System.out.println(park);
+            LatLng parkLoc = new LatLng( 49.271520, -123.197868);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(parkLoc, 15));
+        }
+
+        if(park.equals("Musqueam Park")){
+            System.out.println(park);
+            LatLng parkLoc = new LatLng(49.229412, -123.190269);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(parkLoc, 16));
+        }
 
     }
 
