771283b71c9394202708582f8ed9cbfe64a517a6
MDandaya <MDandaya@gmail.com>
2019-11-10 15:21:39 -0800

    hardcoded points to parks

--- app/src/main/java/com/example/vengersmap/AddArtifact.java
+++ app/src/main/java/com/example/vengersmap/AddArtifact.java
@@ -23,7 +23,7 @@ public class AddArtifact extends FragmentActivity implements OnMapReadyCallback,
     private EditText etArtName;
     private String name = " ";
     private String park;
-    private ArrayList<locations> artLocations;
+    private ArrayList<Location> artLocation;
     public SupportMapFragment mapFragment;
 
 
@@ -43,25 +43,192 @@ public class AddArtifact extends FragmentActivity implements OnMapReadyCallback,
 
     }
 
-    public void createLocations(){
+    public void createLocation(){
         if(park.equals("Strathchona Park")){
-            locations strath1 = new locations();
+            Location strath1 = new Location();
             strath1.setName("Point 1");
             strath1.setX(49.274795);
             strath1.setY(-123.086702);
 
-            locations strath2 = new locations();
+            Location strath2 = new Location();
             strath2.setName("Point 2");
             strath2.setX(49.276052);
             strath2.setY(-123.087088);
 
-            artLocations.add(strath1);
-            artLocations.add(strath2);
+            Location strath3 = new Location();
+            strath3.setName("Point 3");
+            strath3.setX(49.276078);
+            strath3.setY(-123.085570);
+
+            Location strath4 = new Location();
+            strath4.setName("Point 4");
+            strath4.setX(49.275772);
+            strath4.setY(-132.085506);
+
+            Location strath5 = new Location();
+            strath5.setName("Point 5");
+            strath5.setX(49.275561);
+            strath5.setY(-123.085490);
+
+            Location strath6 = new Location();
+            strath6.setName("Point 6");
+            strath6.setX(49.275278);
+            strath6.setY(-123.085662);
+
+            Location strath7 = new Location();
+            strath7.setName("Point 7");
+            strath7.setX(49.274820);
+            strath7.setY(-123.085796);
+
+            Location strath8 = new Location();
+            strath8.setName("Point 8");
+            strath8.setX(49.274542);
+            strath8.setY(-123.085285);
+
+            Location strath9 = new Location();
+            strath9.setName("Point 9");
+            strath9.setX(49.275333);
+            strath9.setY(-123.085105);
+
+            Location strath10 = new Location();
+            strath10.setName("Point 10");
+            strath10.setX(49.275549);
+            strath10.setY(-123.083941);
+
+            artLocation.add(strath1);
+            artLocation.add(strath2);
+            artLocation.add(strath3);
+            artLocation.add(strath4);
+            artLocation.add(strath5);
+            artLocation.add(strath6);
+            artLocation.add(strath7);
+            artLocation.add(strath8);
+            artLocation.add(strath9);
+            artLocation.add(strath10);
         }
         if(park.equals("Jericho Beach Park")){
 
+            Location jericho1 = new Location();
+            jericho1.setName("Point 1");
+            jericho1.setX(49.274828);
+            jericho1.setY(-123.201832);
+            artLocation.add(jericho1);
+
+            Location jericho2 = new Location();
+            jericho2.setName("Point 2");
+            jericho2.setX(49.276294);
+            jericho2.setY(-123.202155);
+            artLocation.add(jericho2);
+
+            Location jericho3 = new Location();
+            jericho3.setName("Point 3");
+            jericho3.setX(49.273879);
+            jericho3.setY(-123.202341);
+            artLocation.add(jericho3);
+
+            Location jericho4 = new Location();
+            jericho4.setName("Point 4");
+            jericho4.setX(49.273739);
+            jericho4.setY(-123.200058);
+            artLocation.add(jericho4);
+
+            Location jericho5 = new Location();
+            jericho5.setName("Point 5");
+            jericho5.setX(49.274113);
+            jericho5.setY(-123.196761);
+            artLocation.add(jericho5);
+
+            Location jericho6 = new Location();
+            jericho6.setName("Point 6");
+            jericho6.setX(49.272621);
+            jericho6.setY(-123.193091);
+            artLocation.add(jericho6);
+
+            Location jericho7 = new Location();
+            jericho7.setName("Point 7");
+            jericho7.setX(49.271104);
+            jericho7.setY(-123.198627);
+            artLocation.add(jericho7);
+
+            Location jericho8 = new Location();
+            jericho8.setName("Point 8");
+            jericho8.setX(49.270938);
+            jericho8.setY(-123.194945);
+            artLocation.add(jericho8);
+
+            Location jericho9 = new Location();
+            jericho9.setName("Point 9");
+            jericho9.setX(49.271108);
+            jericho9.setY(-123.192595);
+            artLocation.add(jericho9);
+
+            Location jericho10 = new Location();
+            jericho10.setName("Point 10");
+            jericho10.setX(49.271781);
+            jericho10.setY(-123.191594);
+            artLocation.add(jericho10);
         }
         if(park.equals("Musqueam Park")){
+
+            Location musq1 = new Location();
+            musq1.setName("Point 1");
+            musq1.setX(49.228619);
+            musq1.setY(-123.188281);
+            artLocation.add(musq1);
+
+            Location musq2 = new Location();
+            musq2.setName("Point 2");
+            musq2.setX(49.229257);
+            musq2.setY(-123.187258);
+            artLocation.add(musq2);
+
+            Location musq3 = new Location();
+            musq3.setName("Point 3");
+            musq3.setX(49.230026);
+            musq3.setY(-123.188071);
+            artLocation.add(musq3);
+
+            Location musq4 = new Location();
+            musq4.setName("Point 4");
+            musq4.setX(49.229189);
+            musq4.setY(-123.189921);
+            artLocation.add(musq4);
+
+            Location musq5 = new Location();
+            musq5.setName("Point 5");
+            musq5.setX(49.229469);
+            musq5.setY(-123.191313);
+            artLocation.add(musq5);
+
+            Location musq6 = new Location();
+            musq6.setName("Point 6");
+            musq6.setX(49.228055);
+            musq6.setY(-123.189591);
+            artLocation.add(musq6);
+
+            Location musq7 = new Location();
+            musq7.setName("Point 7");
+            musq7.setX(49.230150);
+            musq7.setY(-123.193692);
+            artLocation.add(musq7);
+
+            Location musq8 = new Location();
+            musq8.setName("Point 8");
+            musq8.setX(49.229076);
+            musq8.setY(-123.188602);
+            artLocation.add(musq8);
+
+            Location musq9 = new Location();
+            musq9.setName("Point 9");
+            musq9.setX(49.230465);
+            musq9.setY(-123.189165);
+            artLocation.add(musq9);
+
+            Location musq10 = new Location();
+            musq10.setName("Point 10");
+            musq10.setX(49.230319);
+            musq10.setY(-123.188732);
+            artLocation.add(musq10);
 
         }
 
