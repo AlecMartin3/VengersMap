4fa720961bf170a6a4208e3b306265ffd986df7a
Alec Martin <acmartin1@live.ca>
2019-11-09 17:20:35 -0800

    Added firebase connectivity with a login and register page.

--- app/src/main/java/com/example/vengersmap/RegisterActivity.java
+++ app/src/main/java/com/example/vengersmap/RegisterActivity.java
@@ -1,14 +1,65 @@
 package com.example.vengersmap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    Button register;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.Email);
+        password = findViewById(R.id.Password);
+        register = findViewById(R.id.registerBut);
+        firebaseAuth = FirebaseAuth.getInstance();
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                firebaseAuth.createUserWithEmailAndPassword((email.getText().toString()),
+                        password.getText().toString())
+                        .addOnCompleteListener((new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(RegisterActivity.this,
+                                            "Registered Successfully",
+                                            Toast.LENGTH_LONG).show();
+                                    email.setText("");
+                                    password.setText("");
+                                }else{
+                                    Toast.makeText(RegisterActivity.this,
+                                            task.getException().getMessage(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }));
+            }
+        });
+    }
+    public void goToLogin(View view){
+        Intent intent = null;
+        if(view.getId()== R.id.goToLoginBut){
+            intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
