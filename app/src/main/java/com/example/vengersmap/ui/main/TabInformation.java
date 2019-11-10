ad2c62dbe6efb690e57d82db4c1ec87f68710383
MDandaya <MDandaya@gmail.com>
2019-11-10 12:39:35 -0800

    setting text in profile information tab works, must integrate with firebase

--- app/src/main/java/com/example/vengersmap/ui/main/TabInformation.java
+++ app/src/main/java/com/example/vengersmap/ui/main/TabInformation.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.vengersmap.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +34,10 @@ public class TabInformation extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    TextView userEmail;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
 
     private OnFragmentInteractionListener mListener;
 
@@ -61,6 +70,16 @@ public class TabInformation extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+//        userEmail = getView().findViewById(R.id.tvUserEmail);
+//        firebaseAuth = FirebaseAuth.getInstance();
+//        firebaseUser = firebaseAuth.getCurrentUser();
+
+//        userEmail.setText(firebaseUser.getEmail());
+//        userEmail.setText("Testing123");
+
+
+
     }
 
     @Override
@@ -68,6 +87,12 @@ public class TabInformation extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab_information, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        userEmail = getView().findViewById(R.id.tvUserEmail);
+        userEmail.setText("Testing123");
     }
 
     // TODO: Rename method, update argument and hook method into UI event
